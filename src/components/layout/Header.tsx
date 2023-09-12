05753f98ec8d9e3c7c760af53372f7a467b80f20
mrevanzak <mrevanzak@gmail.com>
2023-09-12 18:33:10 +0700

    feat: change header (temporary disable sign in)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
@@ -8,21 +7,19 @@ export default function Header() {
     <header className='sticky top-0 z-50 bg-white'>
       <div className='layout flex h-14 items-center justify-between'>
         <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
+          Tanya.in
         </UnstyledLink>
         <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            <SignedIn>
-              {/* Mount the UserButton component */}
-              <UserButton afterSignOutUrl='/' showName />
-            </SignedIn>
-            <SignedOut>
-              {/* Signed out users get sign in button */}
-              <UnstyledLink href='/sign-in' className='hover:text-gray-600'>
-                Sign in
-              </UnstyledLink>
-            </SignedOut>
-          </ul>
+          {/* <ul className='flex items-center justify-between space-x-4'> */}
+          {/*   <SignedIn> */}
+          {/*     <UserButton afterSignOutUrl='/' showName /> */}
+          {/*   </SignedIn> */}
+          {/*   <SignedOut> */}
+          {/*     <UnstyledLink href='/sign-in' className='hover:text-gray-600'> */}
+          {/*       Sign in */}
+          {/*     </UnstyledLink> */}
+          {/*   </SignedOut> */}
+          {/* </ul> */}
         </nav>
       </div>
     </header>

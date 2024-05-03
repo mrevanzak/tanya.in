509a66ba43296d696e58c59a6edfe889c3c38835
mrevanzak <mrevanzak@gmail.com>
2024-05-03 16:48:25 +0700

    style: fix prettier

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
-import { NextUIProvider, cn } from "@tanya.in/ui";
+import { cn, NextUIProvider } from "@tanya.in/ui";
 import { ThemeProvider, ThemeToggle } from "@tanya.in/ui/theme";
 import { Toaster } from "@tanya.in/ui/toast";
 

35eac8b02f2ff1d29186704c94fb3de637922344
mrevanzak <mrevanzak@gmail.com>
2024-05-06 15:46:00 +0700

    feat(web): add vercel analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
@@ -72,6 +73,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
             <ThemeToggle />
           </div>
           <Toaster />
+          <Analytics />
         </Providers>
       </body>
     </html>

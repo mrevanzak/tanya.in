78a334581541006499b6abfaf37b7a93de2974fd
mrevanzak <mrevanzak@gmail.com>
2024-05-12 22:01:25 +0700

    fix(web): light mode logo color

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -9,7 +9,7 @@ export async function Navbar() {
   return (
     <header className="sticky top-0 z-50 bg-content1 px-8">
       <div className="flex h-16 items-center justify-between">
-        <Logo className="h-9 text-white" />
+        <Logo className="h-9" />
 
         <nav>
           <UserButton user={session?.user} />

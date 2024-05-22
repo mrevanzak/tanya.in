ecbca01d32ef1861111e0ab33e38583d2a2cb259
mrevanzak <mrevanzak@gmail.com>
2024-05-22 16:28:08 +0700

    feat(web): change sidebar color

--- apps/web/src/components/sidebar/sidebar.tsx
+++ apps/web/src/components/sidebar/sidebar.tsx
@@ -25,7 +25,7 @@ export const SidebarWrapper = () => {
       ) : null}
       <div
         className={cn(
-          "fixed z-[202] h-full w-64 shrink-0 -translate-x-full flex-col overflow-y-auto border-r border-divider bg-background px-3 pb-6 transition-transform md:static md:ml-0 md:flex md:h-screen md:translate-x-0 ",
+          "fixed z-[202] h-full w-64 shrink-0 -translate-x-full flex-col overflow-y-auto border-r border-divider bg-content1 px-3 pb-6 transition-transform md:static md:ml-0 md:flex md:h-screen md:translate-x-0 ",
           collapsed && "ml-0 translate-x-0 [display:inherit]",
         )}
       >
@@ -41,10 +41,10 @@ export const SidebarWrapper = () => {
             />
             <SidebarMenu title="Main Menu">
               <SidebarItem
-                title="Files"
+                title="Documents"
                 icon={<IoDocumentText size={24} />}
-                isActive={pathname === "/files"}
-                href="/files"
+                isActive={pathname === "/documents"}
+                href="/documents"
               />
             </SidebarMenu>
           </div>

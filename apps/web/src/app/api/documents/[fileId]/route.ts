4f776b500ab08929cd8ae9343a9cd05975eda4e9
mrevanzak <mrevanzak@gmail.com>
2024-05-27 23:22:34 +0700

    fix(web/server): download file endpoint

--- apps/web/src/app/api/documents/[fileId]/route.ts
+++ apps/web/src/app/api/documents/[fileId]/route.ts
@@ -1,12 +1,14 @@
 import { env } from "@/env";
 import { auth } from "@/server/auth";
 
-export async function GET({ params }: { params: { fileId: string } }) {
+export async function GET(_: Request, context: { params: { fileId: string } }) {
   const session = await auth();
   if (session?.user.role !== "admin") {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  const res = await fetch(env.BACKEND_URL + "/files/download/" + params.fileId);
+  const res = await fetch(
+    env.BACKEND_URL + "/files/download/" + context.params.fileId,
+  );
   return res;
 }

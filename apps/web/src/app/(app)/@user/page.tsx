0c3742ec6aafb02df6eab60980f3892380b1dcbc
mrevanzak <mrevanzak@gmail.com>
2024-06-10 00:17:20 +0700

    feat: change testing mode alert style

--- apps/web/src/app/(app)/@user/page.tsx
+++ apps/web/src/app/(app)/@user/page.tsx
@@ -1,24 +1,9 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/alert";
-import { get } from "@vercel/edge-config";
-
 import { ChatCard } from "./card";
 import { ChatHistory } from "./history";
 
-export default async function HomePage() {
-  const isTesting = await get("testing");
-
+export default function HomePage() {
   return (
     <>
-      {isTesting && (
-        <Alert className="absolute left-0 top-0 my-4 bg-warning text-warning-foreground">
-          <AlertTitle>Testing mode: ON</AlertTitle>
-          <AlertDescription>
-            Sorry, currently the site is in testing mode. Chat responses may not
-            be accurate.
-          </AlertDescription>
-        </Alert>
-      )}
-
       <ChatHistory />
       <div className="container flex-1 self-center">
         <ChatCard />
--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,18 +1,30 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/alert";
+import { SidebarButton } from "@/components/sidebar/sidebar-button";
+import { UserButton } from "@/components/user";
 import { auth } from "@/server/auth";
+import { get } from "@vercel/edge-config";
 
 import { cn } from "@tanya.in/ui";
 
 import Logo from "~/logo.svg";
-import { SidebarButton } from "./sidebar/sidebar-button";
-import { UserButton } from "./user";
 
 export async function Navbar() {
   const session = await auth();
   const isAdmin = session?.user.role === "admin";
+  const isTesting = await get("testing");
 
   return (
-    <header className="sticky top-0 z-10 bg-content1 px-4 sm:px-8">
-      <div className="flex h-16 items-center justify-between">
+    <header className="sticky top-0 z-10 bg-content1">
+      {isTesting && (
+        <Alert className="rounded-none bg-warning text-warning-foreground">
+          <AlertTitle>Testing mode: ON</AlertTitle>
+          <AlertDescription>
+            Sorry, currently the site is in testing mode. Chat responses may not
+            be accurate.
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="flex h-16 items-center justify-between px-4 sm:px-8">
         <Logo
           className={cn("hidden h-7 sm:h-9 md:block", {
             invisible: isAdmin,

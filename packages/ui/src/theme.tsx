b82bb806eb6833db3d86e814b8b59659dee9a3d4
mrevanzak <mrevanzak@gmail.com>
2024-06-13 19:20:31 +0700

    chore: replace radix icon package with react icons

--- packages/ui/src/theme.tsx
+++ packages/ui/src/theme.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { ThemeProvider, useTheme } from "next-themes";
+import { RxMoon, RxSun } from "react-icons/rx";
 
 import { Button } from "./button";
 import {
@@ -19,8 +19,8 @@ function ThemeToggle() {
     <Dropdown classNames={{ content: "min-w-32 bg-default-50" }}>
       <DropdownTrigger>
         <Button variant="bordered" isIconOnly>
-          <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <RxSun className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <RxMoon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownTrigger>

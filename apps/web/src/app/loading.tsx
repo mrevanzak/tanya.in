4d41d11afc6d8667c7b4dc3c212fb5a642d4b893
mrevanzak <mrevanzak@gmail.com>
2024-06-13 22:29:08 +0700

    feat: loading page style improvement

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -3,8 +3,7 @@ import { Spinner } from "@nextui-org/react";
 export default function Loading() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background text-center font-sans text-foreground antialiased ">
-      <Spinner size="lg" />
-      <h3 className="mt-8">Loading...</h3>
+      <Spinner label="Loading..." size="lg" labelColor="primary" />
     </div>
   );
 }

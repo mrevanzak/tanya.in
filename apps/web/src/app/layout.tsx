c617ca21bf56346a44f99cc32e6ed9446de4cae2
mrevanzak <mrevanzak@gmail.com>
2024-06-17 23:09:34 +0700

    feat(web): favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -25,7 +25,15 @@ export const metadata: Metadata = {
   description: siteConfig.description,
   robots: { index: true, follow: true },
   icons: {
-    icon: "/favicon.ico",
+    icon: [{
+      url: "/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    }, {
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    }],
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
@@ -44,7 +52,6 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     images: [`${siteConfig.url}/images/og.jpg`],
-    creator: "@th_clarence",
   },
   authors: [
     {

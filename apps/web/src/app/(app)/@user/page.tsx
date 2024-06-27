e26e7a672f7c4844cbb587503e07974dbbfbf5df
mrevanzak <mrevanzak@gmail.com>
2024-06-27 20:45:38 +0700

    feat: centerized chat container

--- apps/web/src/app/(app)/@user/page.tsx
+++ apps/web/src/app/(app)/@user/page.tsx
@@ -2,7 +2,7 @@ import { ChatCard } from "./card";
 
 export default function HomePage() {
   return (
-    <div className="container flex-1 self-center">
+    <div className="container flex-1 self-center md:-translate-x-6">
       <ChatCard />
     </div>
   );

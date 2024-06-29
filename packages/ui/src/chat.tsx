50223715f11b1c2e38d4ca65b003d6fcf1528e99
mrevanzak <mrevanzak@gmail.com>
2024-06-30 01:32:34 +0700

    fix: message content on dark mode

--- packages/ui/src/chat.tsx
+++ packages/ui/src/chat.tsx
@@ -115,7 +115,7 @@ export function Chat({
               >
                 {item.content.includes("</") && item.content.includes(">") ? (
                   <div
-                    className="prose"
+                    className="prose dark:prose-invert"
                     dangerouslySetInnerHTML={{ __html: item.content }}
                   />
                 ) : (
